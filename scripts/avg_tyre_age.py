79be5f634e4f315478830079ec2842ca7cc71295
L1xus <omarstellar456@gmail.com>
2024-12-02 19:52:48 +0100

    fixed avg_tyre_age and avg_tyre_team scripts

--- scripts/avg_tyre_age.py
+++ scripts/avg_tyre_age.py
@@ -1,38 +1,35 @@
 import json
 
-# Exemple de données JSON
-with open('data/stints.json', 'r') as pos_file:
-    data = json.load(pos_file)
+def avg_tyre_age():
+    # Exemple de données JSON
+    with open('data/stints.json', 'r') as pos_file:
+        data = json.load(pos_file)
 
 
-# Dictionnaire pour stocker les données agrégées
-driver_durations = {}
+    # Dictionnaire pour stocker les données agrégées
+    driver_durations = {}
 
-# Parcourir les sessions
-for session_key, drivers in data.items():
-    for driver_number, laps in drivers.items():
-        # Initialiser les listes pour chaque conducteur si elles n'existent pas encore
-        if driver_number not in driver_durations:
-            driver_durations[driver_number] = {
-                "tyre_age_at_start": []
-            }
-        
-        # Ajouter les durées des secteurs et des tours
-        for lap in laps:
-            if "tyre_age_at_start" in lap and lap["tyre_age_at_start"] is not None:
-                driver_durations[driver_number]["tyre_age_at_start"].append(lap["tyre_age_at_start"])
+    # Parcourir les sessions
+    for session_key, drivers in data.items():
+        for driver_number, laps in drivers.items():
+            # Initialiser les listes pour chaque conducteur si elles n'existent pas encore
+            if driver_number not in driver_durations:
+                driver_durations[driver_number] = {
+                    "tyre_age_at_start": []
+                }
+            
+            # Ajouter les durées des secteurs et des tours
+            for lap in laps:
+                if "tyre_age_at_start" in lap and lap["tyre_age_at_start"] is not None:
+                    driver_durations[driver_number]["tyre_age_at_start"].append(lap["tyre_age_at_start"])
 
 
-# Calculer les moyennes pour chaque conducteur
-driver_tyre_age_avrg = {
-    driver: {
-        "tyre_age": sum(durations["tyre_age_at_start"]) / len(durations["tyre_age_at_start"]) if durations["tyre_age_at_start"] else 0
+    # Calculer les moyennes pour chaque conducteur
+    driver_tyre_age_avg = {
+        driver: {
+            "tyre_age": sum(durations["tyre_age_at_start"]) / len(durations["tyre_age_at_start"]) if durations["tyre_age_at_start"] else 0
+        }
+        for driver, durations in driver_durations.items()
     }
-    for driver, durations in driver_durations.items()
-}
 
-
-with open("avg_tyre_age.json", "w") as json_file:
-    json.dump(driver_tyre_age_avrg, json_file, indent=4)
-
-print(f"Les moyennes des durées ont été enregistrées dans ")
+    return driver_tyre_age_avg
--- scripts/avg_tyre_team.py
+++ scripts/avg_tyre_team.py
@@ -1,36 +1,30 @@
 import json
 
-# Charger les données des pilotes
-with open('data/drivers.json', 'r') as pos_file:
-    drivers_data = json.load(pos_file)
+def avg_tyre_team(stints):
+    # Charger les données des pilotes
+    with open('data/drivers.json', 'r') as pos_file:
+        drivers_data = json.load(pos_file)
 
-# Charger les données des temps moyens d'arrêt
-# Get this from the cloud
-with open('avg_tyre_age.json', 'r') as pos_file:
-    stints = json.load(pos_file)
+    # Création d'un dictionnaire associant chaque driver_number à son team_name
+    driver_team_mapping = {driver['driver_number']: driver['team_name'] for driver in drivers_data}
 
-# Création d'un dictionnaire associant chaque driver_number à son team_name
-driver_team_mapping = {driver['driver_number']: driver['team_name'] for driver in drivers_data}
+    # Création d'un dictionnaire pour stocker les durées par équipe
+    team_tyre_age = {}
 
-# Création d'un dictionnaire pour stocker les durées par équipe
-team_tyre_age = {}
+    # Ajout du team_name aux lap_times et agrégation par équipe
+    for driver_number, times in stints.items():
+        driver_number_int = int(driver_number)  # Convertir la clé en entier pour correspondre au driver_number
+        team_name = driver_team_mapping.get(driver_number_int, "Unknown Team")  # Récupérer le nom de l'équipe
+        age = times["tyre_age"]  # Récupérer la durée moyenne
 
-# Ajout du team_name aux lap_times et agrégation par équipe
-for driver_number, times in stints.items():
-    driver_number_int = int(driver_number)  # Convertir la clé en entier pour correspondre au driver_number
-    team_name = driver_team_mapping.get(driver_number_int, "Unknown Team")  # Récupérer le nom de l'équipe
-    age = times["tyre_age"]  # Récupérer la durée moyenne
+        # Ajouter les durées dans le dictionnaire d'agrégation
+        if team_name not in team_tyre_age:
+            team_tyre_age[team_name] = []
+        team_tyre_age[team_name].append(age)
 
-    # Ajouter les durées dans le dictionnaire d'agrégation
-    if team_name not in team_tyre_age:
-        team_tyre_age[team_name] = []
-    team_tyre_age[team_name].append(age)
+    # Calcul de la moyenne pour chaque équipe
+    team_pit_average = {
+        team: sum(durations) / len(durations) for team, durations in team_tyre_age.items()
+    }
 
-# Calcul de la moyenne pour chaque équipe
-team_pit_average = {
-    team: sum(durations) / len(durations) for team, durations in team_tyre_age.items()
-}
-
-# Enregistrement des résultats dans un fichier JSON
-with open("avrg_tyre_team.json", "w") as json_file:
-    json.dump(team_pit_average, json_file, indent=4)
+    return team_pit_average
