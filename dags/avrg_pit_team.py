9912d782fe27e84fb4d7a89a061d7e8c566ae42f
L1xus <omarstellar456@gmail.com>
2024-12-02 18:27:48 +0100

    transformations added

---
+++ dags/avrg_pit_team.py
@@ -0,0 +1,35 @@
+import json
+
+
+with open('drivers.json', 'r') as pos_file:
+    drivers_data = json.load(pos_file)
+
+# Charger les données des temps moyens d'arrêt
+with open('avrg_pits.json', 'r') as pos_file:
+    pits = json.load(pos_file)
+
+# Création d'un dictionnaire associant chaque driver_number à son team_name
+driver_team_mapping = {driver['driver_number']: driver['team_name'] for driver in drivers_data}
+
+# Création d'un dictionnaire pour stocker les durées par équipe
+team_pit_durations = {}
+
+# Ajout du team_name aux lap_times et agrégation par équipe
+for driver_number, times in pits.items():
+    driver_number_int = int(driver_number)  # Convertir la clé en entier pour correspondre au driver_number
+    team_name = driver_team_mapping.get(driver_number_int, "Unknown Team")  # Récupérer le nom de l'équipe
+    pit_duration = times["average_pit_duration"]  # Récupérer la durée moyenne
+
+    # Ajouter les durées dans le dictionnaire d'agrégation
+    if team_name not in team_pit_durations:
+        team_pit_durations[team_name] = []
+    team_pit_durations[team_name].append(pit_duration)
+
+# Calcul de la moyenne pour chaque équipe
+team_pit_average = {
+    team: sum(durations) / len(durations) for team, durations in team_pit_durations.items()
+}
+
+# Enregistrement des résultats dans un fichier JSON
+with open("avrg_pit_team.json", "w") as json_file:
+    json.dump(team_pit_average, json_file, indent=4)
---
+++ dags/team_lap_duration.py
@@ -0,0 +1,36 @@
+import json
+
+# Données de lap times
+with open('team_duration.json', 'r') as pos_file:
+    lap_times = json.load(pos_file)
+
+# Dictionnaire pour accumuler les durées des tours et les comptes pour chaque équipe
+team_lap_durations = {}
+
+# Parcours des données des pilotes pour accumuler les durées des tours par équipe
+for driver_number, times in lap_times.items():
+    team_name = times["team_name"]  # Récupérer le nom de l'équipe
+    lap_duration = times["average_lap_duration"]
+    
+    # Si lap_duration est valide (supérieur à zéro)
+    if lap_duration > 0:
+        if team_name not in team_lap_durations:
+            team_lap_durations[team_name] = {"total_duration": 0, "count": 0}
+        
+        # Ajouter la durée du tour et augmenter le compteur pour cette équipe
+        team_lap_durations[team_name]["total_duration"] += lap_duration
+        team_lap_durations[team_name]["count"] += 1
+
+# Calcul de la moyenne des durées des tours pour chaque équipe
+team_average_lap_durations = {}
+for team, data in team_lap_durations.items():
+    if data["count"] > 0:
+        average_duration = data["total_duration"] / data["count"]
+        team_average_lap_durations[team] = average_duration
+
+#tri croissant
+sorted_team_average_lap_durations = dict(sorted(team_average_lap_durations.items(), key=lambda item: item[1]))
+
+# Affichage des résultats
+with open("team_lap_duration.json", "w") as json_file:
+    json.dump(sorted_team_average_lap_durations, json_file, indent=4)
